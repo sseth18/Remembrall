7554b2731b65ab579dfed5469dd0ca2ce718c4aa
sseth18 <samar.seth@gmail.com>
2020-01-15 12:10:01 -0500

    Initial Commit

---
+++ Remembrall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Remembrall
+//
+//  Created by Samar Seth on 1/15/20.
+//  Copyright © 2020 Samar Seth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
